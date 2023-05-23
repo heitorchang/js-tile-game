3da0e38e6a789c951f6d4052bd9b27b9cda17964
Heitor Chang <heitorpontual@gmail.com>
2023-05-23 15:54:49 -0300

    color

--- js/sandbox.js
+++ js/sandbox.js
@@ -54,5 +54,126 @@ for (let r = 0; r < 10; r++) {
   m.push(row)
 }
 
-p.style.left = '0px';
-p.style.top = '0px';
+// location and rotation states
+let pRow = 0
+let pCol = 0
+let pRot = 0
+
+function move() {
+  if (pRow < 0 || pRow > 9) {
+    throw Error("Row must be in the range 0-9")
+  }
+  if (pCol < 0 || pCol > 9) {
+    throw Error("Column must be in the range 0-9")
+  }
+  p.style.transform = `translateX(${pCol * 30}px) translateY(${pRow * 30}px) rotate(${pRot}deg)`
+}
+
+function moveTo(r, c) {
+  pRow = r
+  pCol = c
+  move()
+}
+
+function turnR() {
+  pRot += 90
+  move()
+
+}
+
+function turnL() {
+  pRot -= 90
+  move()
+}
+
+function turnTo(cardinalPoint) {
+  switch (cardinalPoint) {
+    case "N":
+      pRot = -90
+      break
+    case "E":
+      pRot = 0
+      break
+    case "S":
+      pRot = 90
+      break
+    case "W":
+      pRot = 180
+      break
+    default:
+      pRot = 0
+  }
+  move()
+}
+
+pCol = 4
+pRow = 9
+turnTo("N")
+move()
+
+function walk(n) {
+  // save original values
+  const oldRow = pRow
+  const oldCol = pCol
+
+  let dir = pRot
+  while (dir < 0) dir += 360
+  // N: 270
+  // E: 0
+  // S: 90
+  // W: 180
+  switch (dir) {
+    case 270:
+      pRow -= n
+      break
+    case 0:
+      pCol += n
+      break
+    case 90:
+      pRow += n
+      break
+    case 180:
+      pCol -= n
+      break
+  }
+  try {
+    move()
+  } catch (e) {
+    console.log("Could not move due to error")
+    console.log(e)
+    pRow = oldRow
+    pCol = oldCol
+  }
+}
+
+function write(s) {
+  // set the current location's text
+  m[pRow][pCol].innerText = s
+}
+
+function read() {
+  // get the current location's text
+  return m[pRow][pCol].innerText
+}
+
+function evalPlayerCode() {
+  eval(document.getElementById("playerCode").value)
+}
+
+function evalStepByStep(lines) {
+  if (lines.length === 0) return
+  eval(lines[0])
+  window.setTimeout(() => {
+    evalStepByStep(lines.slice(1))
+  }, 300)
+}
+
+const samplePlayerCode = `
+walk(3)
+turnR()
+write("O HAI")
+`
+
+function color(c) {
+  m[pRow][pCol].style.backgroundColor = c
+}
